f50813f3eed8fcdcf3e520e0c4e276cda7b32ab8
Ethan Irvin <aethil@yahoo.com>
2023-04-17 21:31:08 -0500

    Studio Complete

--- src/org/launchcode/studio7/CD.java
+++ src/org/launchcode/studio7/CD.java
@@ -1,10 +1,34 @@
 package org.launchcode.studio7;
 
-public class CD {
+import java.util.ArrayList;
 
-    // TODO: Implement your custom interface.
+public class CD extends BaseDisc{
 
-    // TODO: Determine which fields, methods, and constructors can be extended from the base class and which ones
-    //  need to be declared separately.
+    private final double maximumData;
+    public CD(String name, ArrayList<String> files, String discType) {
+        super(name, files, discType);
+        this.maximumData = 700;
+    }
 
+    @Override
+    public void spin() {
+        System.out.println("CD type of disc spins at 1.3 m/s.");
+    }
+
+    @Override
+    public void storeData() {
+        System.out.println("CD has maximum storage type 700MB.");
+    }
+
+    @Override
+    public void writeByLaser() {
+        System.out.println("CD writes by semiconductor laser at a fixed wavelength of 780nm.");
+
+    }
+
+    @Override
+    public void readByLaser() {
+        System.out.println("CD is read by semiconductor laser.");
+
+    }
 }
